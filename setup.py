958f88553978d25d78db20be2a3ee106d929a479
venaturum <venaturum@gmail.com>
2020-03-04 12:46:01 +1100

    LONG DESCRIPTION SET FOR USE WITH PYPI

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
 from setuptools import setup
 import versioneer
 
+with open('README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+    
 requirements = [
     'sortedcontainers>=2',
     'pandas>=0.24',
@@ -13,6 +16,8 @@ setup(
     version=versioneer.get_version(),
     cmdclass=versioneer.get_cmdclass(),
     description="Modelling of quantitative state changes as step functions",
+    long_description=long_description,
+    long_description_content_type='text/markdown'
     license="MIT",
     author="Riley Clement",
     author_email='venaturum@gmail.com',
