a83a4844b0c3f725461155d766d38ebe3d3624de
venaturum <venaturum@gmail.com>
2020-11-09 23:23:43 +1100

    TIMEZONE DOCUMENTATION

--- docs/conf.py
+++ docs/conf.py
@@ -57,7 +57,7 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store',
     'examples/.ipynb_checkpoints',
-    'examples/Case Study Asset Utilisation TZ.ipynb',
+    'articles/.ipynb_checkpoints',
     'examples/Index.ipynb'
 ]
 
