72d2586ac67f75008039b620762e5cec7de78c89
venaturum <venaturum@gmail.com>
2020-11-10 15:42:17 +1100

    MODS TO SETUP.PY DATA

--- setup.py
+++ setup.py
@@ -8,20 +8,26 @@ requirements = [
     'sortedcontainers>=2,<3',
     'pandas>=0.24,<2',
     'numpy>=1.16.0,<2',
-    'matplotlib>3.0.0,<4'
+    'matplotlib>3.0.0,<4',
+    'pytz',
 ]
 
 setup(
     name='staircase',
     version=versioneer.get_version(),
     cmdclass=versioneer.get_cmdclass(),
-    description="Modelling of quantitative state changes as step functions",
+    description="Facilitating the modelling, manipulation and analysis of data with (mathematical) step functions",
     long_description=long_description,
     long_description_content_type='text/markdown',
     license="MIT",
     author="Riley Clement",
     author_email='venaturum@gmail.com',
     url='https://github.com/venaturum/staircase',
+    project_urls={
+        "Bug Tracker": 'https://github.com/venaturum/staircase/issues',
+        "Documentation": 'https://railing.readthedocs.io',
+        "Source Code": 'https://github.com/venaturum/staircase',
+    },
     packages=['staircase', 'staircase.docstrings'],
     python_requires='>=3.6',
     install_requires=requirements,
@@ -36,8 +42,11 @@ setup(
         'Simulation Output',
     ],
     classifiers=[
+        'Operating System :: OS Independent',
+        'Intended Audience :: Science/Research',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Topic :: Scientific/Engineering',
     ]
 )
