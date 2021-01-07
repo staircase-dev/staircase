74338638241b73af31f8195924e6a624b83a6f0c
venaturum <rileyclement@gmail.com>
2021-01-07 13:50:22 +1100

    REFACTORED _MIN_PAIR AND _MAX_PAIR

--- staircase/core/aggregation.py
+++ staircase/core/aggregation.py
@@ -1,64 +1,53 @@
+import operator
 import numpy as np
 import staircase as sc
 
 
-def _min_pair(stairs1, stairs2):
+def _make_extremum_func(extrema):
     """
-    Calculates the minimum of two Stairs objects.  It can be thought of as calculating the minimum of two step functions.
-
-    Parameters:
-        stairs1 (Stairs)
-        stairs2 (Stairs)
-
+    Used to generate _min_pair and _max_pair respectively to cut code duplication
+    
+    Parameters
+    ----------
+    extrema : {"minimum", "maximum"}
+    
     Returns:
-        Stairs: the result of the calculation
-
+        Callable
     """
-    assert isinstance(stairs1, sc.Stairs) and isinstance(
-        stairs2, sc.Stairs
-    ), "Arguments to min must be both of type Stairs."
-    assert stairs1.tz == stairs2.tz
-    new_instance = stairs1 - stairs2
-    cumulative = new_instance._cumulative()
-    for key, value in cumulative.items():
-        if value > 0:
-            cumulative[key] = 0
-    deltas = [cumulative.values()[0]]
-    deltas.extend(np.subtract(cumulative.values()[1:], cumulative.values()[:-1]))
-    new_instance = sc.Stairs(
-        dict(zip(new_instance._keys(), deltas)),
-        use_dates=stairs1.use_dates or stairs2.use_dates,
-        tz=stairs1.tz,
-    )
-    return new_instance + stairs2
+    comparator = {"minimum": operator.gt, "maximum": operator.lt}[extrema]
+
+    def extremum_func(stairs1, stairs2):
+        f"""
+        Calculates the {extrema} of two Stairs objects.  It can be thought of as calculating the {extrema} of two step functions.
+
+        Parameters:
+            stairs1 (Stairs)
+            stairs2 (Stairs)
+
+        Returns:
+            Stairs: the result of the calculation
+
+        """
+        assert isinstance(stairs1, sc.Stairs) and isinstance(
+            stairs2, sc.Stairs
+        ), "Arguments must be both of type Stairs."
+        assert stairs1.tz == stairs2.tz
+        new_instance = stairs1 - stairs2
+        cumulative = new_instance._cumulative()
+        for key, value in cumulative.items():
+            if comparator(value, 0):
+                cumulative[key] = 0
+        deltas = [cumulative.values()[0]]
+        deltas.extend(np.subtract(cumulative.values()[1:], cumulative.values()[:-1]))
+        new_instance = sc.Stairs(
+            dict(zip(new_instance._keys(), deltas)),
+            use_dates=stairs1.use_dates or stairs2.use_dates,
+            tz=stairs1.tz,
+        )
+        return new_instance + stairs2
+
+    return extremum_func
 
 
-def _max_pair(stairs1, stairs2):
-    """
-    Calculates the maximum of two Stairs objects.  It can be thought of as calculating the maximum of two step functions.
-
-    Parameters:
-        stairs1 (Stairs)
-        stairs2 (Stairs)
-
-    Returns:
-        Stairs: the result of the calculation
-
-    """
-    assert isinstance(stairs1, sc.Stairs) and isinstance(
-        stairs2, sc.Stairs
-    ), "Arguments to max must be both of type Stairs."
-    assert stairs1.tz == stairs2.tz
-    new_instance = stairs1 - stairs2
-    cumulative = new_instance._cumulative()
-    for key, value in cumulative.items():
-        if value < 0:
-            cumulative[key] = 0
-    deltas = [cumulative.values()[0]]
-    deltas.extend(np.subtract(cumulative.values()[1:], cumulative.values()[:-1]))
-    new_instance = sc.Stairs(
-        dict(zip(new_instance._keys(), deltas)),
-        use_dates=stairs1.use_dates or stairs2.use_dates,
-        tz=stairs1.tz,
-    )
-    return new_instance + stairs2
+_min_pair = _make_extremum_func("minimum")
+_max_pair = _make_extremum_func("maximum")
