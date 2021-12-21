64205304f3fe38a8a666b262a5e587c5b2f5453d
Venaturum <venaturum@gmail.com>
2021-12-21 14:21:35 +1100

    Typo in StairsDtype

--- staircase/core/arrays/extension.py
+++ staircase/core/arrays/extension.py
@@ -31,7 +31,7 @@ class StairsDtype(ExtensionDtype):
         if string == cls.name:
             return cls()
         else:
-            raise TypeError("Cannot construct a '{}' from " "'{}'".format(cls, string))
+            raise TypeError("Cannot construct a '{}' from '{}'".format(cls, string))
 
     @classmethod
     def construct_array_type(cls):
